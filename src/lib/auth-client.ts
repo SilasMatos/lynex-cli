ccc11047578999ec30cd31b535a0330003f19d11
Silas <silas.progamacao@gmail.com>
2026-03-06 09:23:03 -0300

    Adding the API URL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://lynex-api-production.up.railway.app',
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
