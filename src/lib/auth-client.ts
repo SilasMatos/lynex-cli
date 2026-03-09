51670bd05b2067fa805e9425194ed0f4327839e5
Silas <silas.progamacao@gmail.com>
2026-03-09 00:54:51 -0300

    .

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,13 +1,11 @@
-import { createAuthClient } from 'better-auth/react'
-import { oAuthProxyClient } from 'better-auth/client/plugins'
+import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-  baseURL: 'https://lynex-api-production.up.railway.app',
-  basePath: '/auth',
+  baseURL: "https://lynex-api-production.up.railway.app",
+  basePath: "/auth",
   fetchOptions: {
-    credentials: 'include',
+    credentials: "include",
   },
-  plugins: [oAuthProxyClient()],
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
