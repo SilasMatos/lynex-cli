9a79d97a840b5a91911cb61c90ef02a8a739519d
Silas <silas.progamacao@gmail.com>
2026-03-09 00:31:31 -0300

    added   fetchOptions include

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,9 @@ import { createAuthClient } from 'better-auth/react'
 export const authClient = createAuthClient({
   baseURL: 'https://lynex-api-production.up.railway.app',
   basePath: '/auth',
+  fetchOptions: {
+    credentials: 'include',
+  },
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
