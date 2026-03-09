c840822e951758b5cd5b0da45346614d58f6491c
Silas <silas.progamacao@gmail.com>
2026-03-08 21:35:43 -0300

    retirado base path

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,6 @@ import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
   baseURL: 'https://lynex-api-production.up.railway.app',
-  basePath: '/auth',
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
